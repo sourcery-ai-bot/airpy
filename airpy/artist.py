d475b8a45ded02e3657b00059164c41254f197d5
Brandon Locke <git@brandonllocke.com>
2020-04-23 19:07:37 -0400

    removed apitest.py

--- airpy/artist.py
+++ airpy/artist.py
@@ -1,4 +1,4 @@
-from .album import Album
+from album import Album
 
 class Artist:
     def __init__(self, info):
@@ -7,7 +7,7 @@ class Artist:
         self.cover_art = info.get('coverArt')
         self.album_count = info.get('albumCount')
         self.albums = self.parse_albums(info.get('album'))
-        self.albums_by_release = self.albums_by_release()
+        self.albums_by_release = self.parse_albums_by_release()
 
     def parse_albums(self, albums):
         parsed_albums = []
@@ -15,7 +15,7 @@ class Artist:
             parsed_albums.append(Album(album))
         return parsed_albums
 
-    def albums_by_release(self):
+    def parse_albums_by_release(self):
         return sorted(self.albums, key=lambda x: x.year)
         
 
